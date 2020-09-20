089a3f92a4a9358d927cee286c23ec35f1546e24
King <kbmajor80@gmail.com>
2020-09-19 22:42:54 -0400

    Updates

---
+++ precache-manifest.86402bdb2f3cb7465081c40cb099baa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353a12202f03eaa8f9c579d8c890b628",
+    "url": "/react-todo-remake/index.html"
+  },
+  {
+    "revision": "9ad2d497a620ebdc08bf",
+    "url": "/react-todo-remake/static/css/main.d40dfe5b.chunk.css"
+  },
+  {
+    "revision": "0318d11e2a7c16fd4f1d",
+    "url": "/react-todo-remake/static/js/2.f6673008.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-remake/static/js/2.f6673008.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad2d497a620ebdc08bf",
+    "url": "/react-todo-remake/static/js/main.cc4e2a81.chunk.js"
+  },
+  {
+    "revision": "7f61c2de107097320904",
+    "url": "/react-todo-remake/static/js/runtime-main.42174a7b.js"
+  }
+]);
